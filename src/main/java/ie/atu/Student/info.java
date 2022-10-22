2b93a950b7dc4095a6d5ab0e602108da21bcdcce
G00402034 <G00402034@atu.ie>
2022-10-22 19:02:30 +0100

    added new class called info

---
+++ src/main/java/ie/atu/Student/info.java
@@ -0,0 +1,32 @@
+package ie.atu.Student;
+
+public class info {
+
+    private String Student_Name;
+    private String Student_Email;
+    private String Student_Course;
+
+    public String getStudent_Name() {
+        return Student_Name;
+    }
+
+    public void setStudent_Name(String student_Name) {
+        Student_Name = student_Name;
+    }
+
+    public String getStudent_Email() {
+        return Student_Email;
+    }
+
+    public void setStudent_Email(String student_Email) {
+        Student_Email = student_Email;
+    }
+
+    public String getStudent_Course() {
+        return Student_Course;
+    }
+
+    public void setStudent_Course(String student_Course) {
+        Student_Course = student_Course;
+    }
+}
