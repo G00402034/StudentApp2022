5c477d816194843b428ec70017e3b6a35d6d8998
G00402034 <G00402034@atu.ie>
2022-10-22 16:38:32 +0100

    added prompt for name/email/course

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,6 +1,22 @@
 package ie.atu;
-
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
+
+        System.out.println("please enter student name");
+        Scanner input = new Scanner(System.in);
+        String Student_Name = input.next();
+        System.out.println("You entered :" + Student_Name );
+
+        System.out.println("please enter student email");
+        String Student_Email = input.next();
+        System.out.println("You entered :" + Student_Email );
+
+        System.out.println("please enter your course title");
+        String Student_Course = input.next();
+        System.out.println("You entered :" + Student_Course );
+
+
     }
     }
